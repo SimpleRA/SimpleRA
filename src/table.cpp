967ab6374cfec5fcde148c5e7ddf05fed4357472
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-08-28 07:12:19 +0000

    Mximum Rows in Page fixed

--- src/table.cpp
+++ src/table.cpp
@@ -38,7 +38,7 @@ Table::Table(string tableName, vector<string> columns)
     this->tableName = tableName;
     this->columns = columns;
     this->columnCount = columns.size();
-    this->maxRowsPerBlock = (uint)((BLOCK_SIZE * 1000) / (32 * columnCount));
+    this->maxRowsPerBlock = (uint)((BLOCK_SIZE * 1000) / (4 * columnCount));
     this->writeRow<string>(columns);
 }
 
@@ -90,7 +90,7 @@ bool Table::extractColumnNames(string firstLine)
         this->columns.emplace_back(word);
     }
     this->columnCount = this->columns.size();
-    this->maxRowsPerBlock = (uint)((BLOCK_SIZE * 1000) / (32 * this->columnCount));
+    this->maxRowsPerBlock = (uint)((BLOCK_SIZE * 1000) / (4 * this->columnCount));
     return true;
 }
 
